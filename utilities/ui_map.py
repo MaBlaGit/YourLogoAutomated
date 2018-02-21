0fb0afccc65f0c5c97480f97d05aac430ceea256
Mariusz <mariuszblajet@gmail.com>
2018-02-21 17:16:55 +0100

    adding page objects and simple test for checking functionalities

--- utilities/ui_map.py
+++ utilities/ui_map.py
@@ -8,5 +8,5 @@ product_page = dict(
     selectProductByClassName="replace-2x",
     hoverOverProductBoxByClassName="right-block",
     productAddButtonByXpath="//span[contains(text(), 'Add to cart')]",
-    modalWindowByXpath="/html/body/div[1]/div[1]/header/div[3]/div/div/div[4]/div[1]/div[2]/div[4]/span/span"
+    modalWindowByXpath="//i[@class='icon-chevron-left left']"
 )
